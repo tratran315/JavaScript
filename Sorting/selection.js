b203c5621a95d2d76a107da488a2239c36e36b15
tratran315 <tratran.315@gmail.com>
2023-10-16 17:06:24 +0200

    Selection sort

---
+++ Sorting/selection.js
@@ -0,0 +1,20 @@
+const numbers = [9, 4, 13, 2, 20, -1, 0];
+
+const selectionSort = function (arr) {
+  const copiedArr = [...arr];
+  for (let i = 0; i < copiedArr.length; i++) {
+    let min = copiedArr[i];
+    let minPosition = i;
+    for (let j = i + 1; j < copiedArr.length; j++) {
+      if (min > copiedArr[j]) {
+        min = copiedArr[j];
+        minPosition = j;
+      }
+    }
+    const temp = copiedArr[i];
+    copiedArr[i] = copiedArr[minPosition];
+    copiedArr[minPosition] = temp;
+  }
+  return copiedArr;
+};
+console.log(selectionSort(numbers));
