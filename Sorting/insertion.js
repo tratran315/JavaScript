7089df51f4298ca56f032d3e83c973f7598ff392
tratran315 <tratran.315@gmail.com>
2023-10-16 17:17:38 +0200

    Insertion sort

---
+++ Sorting/insertion.js
@@ -0,0 +1,11 @@
+const numbers = [9, 4, 13, 2, 20, -1, 0];
+
+const insertionSort = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let j = i + 1;
+    while (j > 0) {
+      //TODO compare and move back
+      j--;
+    }
+  }
+};
